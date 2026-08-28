f75d71cb2000cb5f1954e09907a78cd8150f1a5d
Matt Delacour <github.com.referee982@passmail.com>
2026-08-28 13:11:44 -0400

    fix: allow scrolling past the end of the note (#69)

--- apps/web/src/lib/Editor.svelte
+++ apps/web/src/lib/Editor.svelte
@@ -124,7 +124,7 @@
 						fontFamily: 'var(--font-serif)'
 					},
 					'.cm-content': {
-						padding: '1.5rem 0',
+						padding: '1.5rem 0 50vh',
 						fontFamily: 'inherit',
 						lineHeight: '1.7',
 						color: 'var(--fg)',
